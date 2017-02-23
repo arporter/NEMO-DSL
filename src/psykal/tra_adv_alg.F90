983fa0dc881a84901a3ede0a5b223fc0cbf74c56
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-02-23 15:15:21 +0000

    #9 Begin move to field objects

--- src/psykal/tra_adv_alg.F90
+++ src/psykal/tra_adv_alg.F90
@@ -14,17 +14,25 @@ PROGRAM tra_adv
    !> Modules from the dl-esm-inf library
    USE kind_params_mod, only: wp
    USE grid_mod
+   USE field_mod
    implicit none
-   REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:,:) :: t3sn, t3ns, t3ew, t3we
-   REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:)   :: tsn 
-   REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:)   :: pun, pvn, pwn
-   REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:)   :: mydomain, zslpx, zslpy, zwx, zwy, umask, vmask, tmask, zind
-   REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:)     :: ztfreez, rnfmsk, upsmsk
-   REAL*8, ALLOCATABLE, SAVE, DIMENSION(:)       :: rnfmsk_z
+   REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:) :: tmask_input
+   !> 'Now' ocean temperature/salinity (in NEMO this is a 4D array with the fourth
+   !! dimension holding the tracer index)
+   type(r3d_field) :: tsn 
+   !REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:)   :: tsn 
+   !> Three ocean velocity components
+   type(r3d_field) :: pun, pvn, pwn
+   !REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:)   :: pun, pvn, pwn
+   type(r3d_field) :: mydomain, zslpx, zslpy, zwx, zwy, zind
+   !REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:,:) :: mydomain, zslpx, zslpy, zwx, zwy, umask, vmask, tmask, zind
+   type(r2d_field) :: ztfreez, rnfmsk, upsmsk
+   !REAL*8, ALLOCATABLE, SAVE, DIMENSION(:,:) :: ztfreez, rnfmsk, upsmsk
+   type(r1d_field) :: rnfmsk_z
+   !REAL*8, ALLOCATABLE, SAVE, DIMENSION(:) :: rnfmsk_z
    REAL*8                                        :: zice, zu, z0u, zzwx, zv, z0v, zzwy, ztra, zalpha
-   REAL*8                                        :: r
    REAL*8                                        :: zw, z0w
-   INTEGER                                       :: jpi, jpj, jpk, ji, jj, jk, jt
+   INTEGER                                       :: jpi, jpj, jpk, jt
    INTEGER*8                                     :: it
    CHARACTER(len=10)                             :: env
    !> Timer indexes, one for initialisation, one for the 'time-stepping'
@@ -53,25 +61,6 @@ PROGRAM tra_adv
 
    call timer_start(init_timer)
 
-   ALLOCATE( mydomain (jpi,jpj,jpk))
-   ALLOCATE( zwx (jpi,jpj,jpk))
-   ALLOCATE( zwy (jpi,jpj,jpk))
-   ALLOCATE( zslpx (jpi,jpj,jpk))
-   ALLOCATE( zslpy (jpi,jpj,jpk))
-   ALLOCATE( pun (jpi,jpj,jpk))
-   ALLOCATE( pvn (jpi,jpj,jpk))
-   ALLOCATE( pwn (jpi,jpj,jpk))
-   ALLOCATE( umask (jpi,jpj,jpk))
-   ALLOCATE( vmask (jpi,jpj,jpk))
-   ALLOCATE( tmask (jpi,jpj,jpk))
-   ALLOCATE( zind (jpi,jpj,jpk))
-   ALLOCATE( ztfreez (jpi,jpj))
-   ALLOCATE( rnfmsk (jpi,jpj))
-   ALLOCATE( upsmsk (jpi,jpj))
-   ALLOCATE( rnfmsk_z (jpk))
-   ALLOCATE( tsn(jpi,jpj,jpk))
-
-
    ! Create the model grid. We use a NE offset (i.e. the U, V and F
    ! points immediately to the North and East of a T point all have the
    ! same i,j index).  This is the same offset scheme as used by NEMO.
@@ -80,47 +69,38 @@ PROGRAM tra_adv
                           (/BC_EXTERNAL,BC_EXTERNAL,BC_NONE/), &
                           OFFSET_NE)
 
-   tmask(:,:,:) = 1.0d0 ! all inner cells
+   ! Generate our 'input' T mask
+   allocate(tmask_input(jpi,jpj,jpk))
+   tmask_input(:,:,:) = 1.0d0 ! all inner cells
 
    ! Having specified the T points mask, we can set up mesh parameters
-   call grid_init(model_grid, jpi, jpj, jpk, dx, dy, dz, tmask)
+   call grid_init(model_grid, jpi, jpj, jpk, dx, dy, dz, tmask_input)
 
-   ! arrays initialization
+   !> Example tracer field (e.g. temperature or salinity)
+   tsn = r3d_field(model_grid, T_POINTS)
+   mydomain = r3d_field(model_grid, T_POINTS)
+   !> \todo Check that I've got the grid-point types right for these
+   zwx = r3d_field(model_grid, U_POINTS)
+   zwy = r3d_field(model_grid, V_POINTS)
+   zslpx = r3d_field(model_grid, U_POINTS)
+   zslpy = r3d_field(model_grid, V_POINTS)
+   !> Ocean velocity components
+   pun = r3d_field(model_grid, U_POINTS)
+   pvn = r3d_field(model_grid, V_POINTS)
+   !> \todo Where do vertical components live on the grid?
+   pwn = r3d_field(model_grid, T_POINTS)
+   !> \todo This is a workspace array; I don't yet know to what it corresponds
+   zind = r3d_field(model_grid, T_POINTS)
+   !> River mouth runoff mask (horiz)
+   rnfmsk = r2d_field(model_grid, T_POINTS)
+   !> River mouth runoff mask (vert)
+   rnfmsk_z = r1d_field(model_grid)
+   !> \todo Work out what these three fields represent
+   ztfreez = r2d_field(model_grid, T_POINTS)
+   upsmsk = r2d_field(model_grid, T_POINTS)
+   tsn = r3d_field(model_grid, T_POINTS)
 
-   r = jpi*jpj*jpk
-
-   ! the following three lines can be uncommented to randomize arrays initialization
-   !call random_seed()
-   !call random_number(r)
-   !r = r*jpi*jpj*jpk
-
-   DO jk = 1, jpk
-      DO jj = 1, jpj
-          DO ji = 1, jpi
-              umask(ji,jj,jk) = ji*jj*jk/r
-              mydomain(ji,jj,jk) =ji*jj*jk/r
-              pun(ji,jj,jk) =ji*jj*jk/r
-              pvn(ji,jj,jk) =ji*jj*jk/r
-              pwn(ji,jj,jk) =ji*jj*jk/r
-              vmask(ji,jj,jk)= ji*jj*jk/r
-              tsn(ji,jj,jk)= ji*jj*jk/r
-              tmask(ji,jj,jk)= ji*jj*jk/r
-          END DO
-      END DO
-   END DO
-
-   r = jpi*jpj
-   DO jj=1, jpj
-      DO ji=1, jpi
-         ztfreez(ji,jj) = ji*jj/r
-         upsmsk(ji,jj) = ji*jj/r
-         rnfmsk(ji,jj) = ji*jj/r
-      END DO
-   END DO
-
-   DO jk=1, jpk
-      rnfmsk_z(jk)=jk/jpk
-   END DO
+   call init_fields()
 
    call timer_stop(init_timer)
 
@@ -158,36 +138,67 @@ PROGRAM tra_adv
 
   call timer_stop(step_timer)
 
-  OPEN(unit = 4, file = 'output.dat', form='formatted')
-  
-  DO jk = 1, jpk-1
-     DO jj = 2, jpj-1
-        DO ji = 2, jpi-1
-           write(4,*) mydomain(ji,jj,jk)
-        END DO
-     END DO
-  END DO
-
-  CLOSE(4)
-
-  DEALLOCATE( mydomain )
-  DEALLOCATE( zwx )
-  DEALLOCATE( zwy )
-  DEALLOCATE( zslpx )
-  DEALLOCATE( zslpy )
-  DEALLOCATE( pun )
-  DEALLOCATE( pvn )
-  DEALLOCATE( pwn )
-  DEALLOCATE( umask)
-  DEALLOCATE( vmask)
-  DEALLOCATE( tmask)
-  DEALLOCATE( zind )
-  DEALLOCATE( ztfreez )
-  DEALLOCATE( rnfmsk)
-  DEALLOCATE( upsmsk)
-  DEALLOCATE( rnfmsk_z)
-  DEALLOCATE( tsn)
+  call write_field(mydomain)
 
   CALL timer_report()
+
+CONTAINS
+
+  subroutine init_fields()
+    REAL(wp) :: r
+    integer :: ji, jj, jk
+
+    ! Array initialization
+    r = jpi*jpj*jpk
+
+    ! the following three lines can be uncommented to randomize the array
+    ! initialization
+    !call random_seed()
+    !call random_number(r)
+    !r = r*jpi*jpj*jpk
+
+   DO jk = 1, jpk
+      DO jj = 1, jpj
+          DO ji = 1, jpi
+              mydomain%data(ji,jj,jk) =ji*jj*jk/r
+              pun%data(ji,jj,jk) =ji*jj*jk/r
+              pvn%data(ji,jj,jk) =ji*jj*jk/r
+              pwn%data(ji,jj,jk) =ji*jj*jk/r
+              tsn%data(ji,jj,jk)= ji*jj*jk/r
+          END DO
+      END DO
+   END DO
+
+   r = jpi*jpj
+   DO jj=1, jpj
+      DO ji=1, jpi
+         ztfreez%data(ji,jj) = ji*jj/r
+         upsmsk%data(ji,jj) = ji*jj/r
+         rnfmsk%data(ji,jj) = ji*jj/r
+      END DO
+   END DO
+
+   DO jk=1, jpk
+      rnfmsk_z%data(jk)=jk/jpk
+   END DO
+
+  end subroutine init_fields
+
+  subroutine write_field(mydomain)
+    type(r3d_field), intent(in) :: mydomain
+    integer :: ji, jj, jk
+
+    OPEN(unit = 24, file = 'output.dat', form='formatted')
+  
+    DO jk = 1, jpk-1
+       DO jj = 2, jpj-1
+          DO ji = 2, jpi-1
+             write(24,*) mydomain%data(ji,jj,jk)
+          END DO
+       END DO
+    END DO
+
+    CLOSE(24)
+  end subroutine write_field
 
 END PROGRAM tra_adv
